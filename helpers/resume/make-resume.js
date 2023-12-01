7ac5375d14c4200838943666cab7a15bc53f1d92
mrsnailo <shahidparvez.official@gmail.com>
2023-12-01 12:48:07 +0600

    fixed resume builder

--- helpers/resume/make-resume.js
+++ helpers/resume/make-resume.js
@@ -8,7 +8,7 @@ const markdown = require('markdown-it')({
   const matter = require('gray-matter');
   
   (async () => {
-	const browser = await puppeteer.launch({headless: "new"});
+	const browser = await puppeteer.launch({headless: 'new'});
 	const page = await browser.newPage();
 	
 	const markdownFilePath = 'content/about/index.md'; // Replace this with the path to your Markdown file
@@ -17,9 +17,47 @@ const markdown = require('markdown-it')({
 	  const fileContent = fs.readFileSync(markdownFilePath, 'utf-8');
 	  const { content } = matter(fileContent);
 	  
-	  fs.writeFileSync("static/downloads/resume.html", markdown.render(content), 'utf-8');
-	  await page.setContent(markdown.render(content));
-	  await page.pdf({ path: 'static/downloads/output.pdf', format: 'A4' });
+	  const htmlContent = markdown.render(content);
+  
+	  // Overlay a div with background color
+	  const htmlWithStyles = `
+		<html>
+		<head>
+		  <style>
+			html {
+			  font-size: 80%;
+			}
+			body{
+			  background: #363944;
+			  color: white;
+			}
+			a {
+			  text-decoration: none;
+			  color: #48b0b5;
+			}
+			code {
+			  color: rgba(#06b0c3,1);
+			  font-weight: 600;
+			  font-size: .875em;
+			  background-color: #77c3c3;
+			  padding-top: 3px;
+			  padding-bottom: 3px;
+			  padding-left: 5px;
+			  padding-right: 5px;
+			  border-radius: .25rem;
+			}
+			
+		  </style>
+		</head>
+		<body>
+		  <div class="background"></div> <!-- Colored div -->
+		  ${htmlContent} <!-- Rendered Markdown content -->
+		</body>
+		</html>
+	  `;
+  
+	  await page.setContent(htmlWithStyles);
+	  await page.pdf({ path: 'static/downloads/resume.pdf', format: 'A4', printBackground: true }); // Ensure printBackground is set to true
 	  
 	  console.log('PDF generated successfully!');
 	} catch (error) {
